64c3c183f5ce398e1f069001c76f670752fc17b7
marsolmos <m.marsolmos@gmail.com>
2020-09-12 00:22:10 +0200

    hide navbar when scrolling down

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// When the user scrolls down, hide the navbar.
+//When the user scrolls up, show the navbar.
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 // Close responsive hamburger menu when click on menu item
 var menu = document.getElementById('responsive-menu');
     var closeIcon = document.getElementById("closeIcon");
